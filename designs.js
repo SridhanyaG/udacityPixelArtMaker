87b53e37cb7239f231beedf26f9ebd48186409f8
Sridhanya Ganapathi <sridhanya.ganapathi@sungardps.com>
2017-10-26 20:16:02 -0700

    implementation

--- designs.js
+++ designs.js
@@ -1,26 +1,26 @@
-$(function() {
+$(function () {
 	// Select color input
 	// Select size input
 
 	// When size is submitted by the user, call makeGrid()
-	$("input[type='submit']").click(function(evt) {
+	$("input[type='submit']").click(function (evt) {
 		makeGrid();
 		evt.preventDefault();
 	});
 
-	$("#pixel_canvas").on("click", "td", function(evt) {
-		$(this).css( "background-color", $("#colorPicker").val());
+	$("#pixel_canvas").on("click", "td", function (evt) {
+		$(this).css("background-color", $("#colorPicker").val());
 	});
 
 	function makeGrid() {
-		let container = $( "#pixel_canvas" );
+		let container = $("#pixel_canvas");
 		container.empty();
 		let rowCount = $("#input_height").val();
 		let columnCount = $("#input_width").val();
-		
-		for (let row=0; row<rowCount; row++) {
+
+		for (let row = 0; row < rowCount; row++) {
 			let row = $("<tr>");
-			for (let col=0; col<columnCount; col++) {
+			for (let col = 0; col < columnCount; col++) {
 				let column = $("<td>");
 				row.append(column);
 			}
